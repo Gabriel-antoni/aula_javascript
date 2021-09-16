c23e60f204c321bfae6dec46cf633f5f071e6b30
Gabriel <66729017+Gabriel-antoni@users.noreply.github.com>
2021-09-16 19:08:32 -0300

    aula javaScript

---
+++ js/main.js
@@ -0,0 +1,119 @@
+/*
+function clicou() {
+  document.getElementById('agradeco').innerHTML = "obrigado por clicar";
+  //alert("obrigado por clicar")
+}
+
+function redirecionar(){
+  window.open("https://globallabs.academy/");
+  //window.location.href = "https://globallabs.academy/";
+}
+
+function trocar(elemento){
+  //document.getElementById("mouseouver");innerHTML = "obrigado por passar o mouse";
+  elemento.innerHTML = "obrigado por pasar o mouse"
+  //alert("trocar texto");
+}
+
+function valor(elemento){
+  //document.getElementById("mouseouver");innerHTML = "obrigado por passar o mouse";
+  elemento.innerHTML = "obrigado por pasar o mouse"
+  //alert("trocar texto");
+}
+
+function load(){
+  alert("pagina carregada");
+}
+
+function funcaoChange(elemento){
+  console.log(elemento.value)
+}*/
+
+
+/*function soma (n1, n2){
+  return n1 + n2;
+}
+
+function validadeIdade(idade){
+  var validar;
+  if (idade >= 18){
+    validar = true
+  }else{
+    validar = false
+  }
+  return validar
+}
+
+var idade = prompt('qual sua idade')
+alert(validadeIdade(idade));*/
+
+/*function soma (n1, n2){
+  return n1 + n2;
+};
+alert(soma(5, 10));
+
+function setReplace(frase, nome, novo_nome){
+  return frase.replace(nome, novo_nome)
+}
+
+alert(setReplace("vai Japao", "Japao", "Brasil"));*/
+
+
+//condicionais, laços de repetição e date
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+*/
+/*
+var count;
+for(count=0; count <=5; count++){
+  alert(count);
+};*/
+
+/*var count = 5;
+while (count < 5){
+  console.log(count);
+  //alert(count);
+  count = count + 1;
+};*/
+/*
+var idade = prompt("informe sua idade?");
+if (idade >= 18){
+alert("maior de idade");
+}else{
+  alert('menor de idade')
+};
+*/
+
+//Arrays e Dicionarios
+
+//dicionario
+
+/*var frutas = [{nome: "maça", cor: "vermelho"}, {nome:"uva", cor: "roxo"}];
+console.log(frutas[1].nome);
+alert(frutas[1].cor);*/
+
+/*var fruta = {nome:"maça", cor: "vermelho"}
+console.log(fruta.nome)
+alert(fruta.cor)*/
+
+//array
+//var lista = ["maça", "pêra", "uva", "laranja"];
+//lista.push("cereja"); //<--adicionar elemento
+//lista.pop("maça"); //<-- retirar elemento
+
+//console.log(lista); //<--exibe a lista no console
+
+//console.log(lista.length); //<-- exibe a quantidade de elementos que tem na lista
+//alert(lista[2])
+//console.log(lista[3])
+//console.log(" - ") //<-- substitui as virgulas pelo traço
+
+
+//Varieaveis e concatenação
+/*var nome = 'gabriel antonio'
+var idade = 22 + ' anos +';
+var idade2 = 23;
+console.log(nome + " tem " + idade + ' e daqui a alguns meses, vai fazer ' + idade2 )
+alert(" ola " + nome);*/
